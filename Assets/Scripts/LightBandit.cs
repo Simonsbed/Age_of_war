2cd9d4f37f913f73e288fa42415565dd25daa3b7
MetoZec <87604547+Simonsbed@users.noreply.github.com>
2024-12-11 14:08:50 +0900

    Level fix

--- Assets/Scripts/LightBandit.cs
+++ Assets/Scripts/LightBandit.cs
@@ -4,5 +4,33 @@ using UnityEngine;
 
 public class LightBandit : Bandit
 {
-    // virtual , override using
+    //Manager 만들 예정 
+    public int level;
+    public float maxExp;
+    public float curExp;
+    public int attLevel;
+    public int hpLevel;
+    public int speedLevel;
+    public int speedLevel;
+    public int coolDownLevel;
+    public float getExpLevel;
+    public int getGoldLevel;
+
+    public void GetExp(float exp){
+        curExp += exp;
+        if (curExp >= maxExp) {
+            LevelUp();
+        }
+        PlayerPrefs.SetFloat("MaxExp", maxExp);
+        PlayerPrefs.SetFloat("CurExp", curExp);
+
+        public void LevelUp(){
+            level += 1;
+            maxExp *= 2;
+            curExp = 0;
+            PlayerPrefs.SetInt("Level", level);
+            PlayerPrefs.SetInt("MaxExp", maxExp);
+            PlayerPrefs.SetInt("CurExp", curExp);
+        }
+    }
 }
